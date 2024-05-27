13637cc831e3e5a98148a172780ac36128c13993
angeladan <a25980@svalero.com>
2024-05-27 19:22:34 +0200

    Actualizado front

--- web/javascript/login.js
+++ web/javascript/login.js
@@ -17,7 +17,7 @@ function sendDataToServer() {
             } else if (response.accountLogged) {
                 sessionStorage.setItem('email', correo);
 
-                window.location.href = "http://localhost:8080/AnBurguer/index.html";
+                window.location.href = "http://localhost:8080/AnBurguer/html/index.html";
             } else {
                 document.getElementById('error-message').style.display = 'block';
                 document.getElementById('error-message').innerText = "No ha sido posible iniciar sesión, pruebe de nuevo";
